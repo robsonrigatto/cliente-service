144abf832e45256361cca4c94ee523ee3afac9ab
robsonrigatto <rigatto.robson@gmail.com>
2020-03-13 09:48:15 -0300

    hystrix dependency + ribbon configuration

--- src/main/java/br/com/rr/mastertech/cliente/ClienteServiceApplication.java
+++ src/main/java/br/com/rr/mastertech/cliente/ClienteServiceApplication.java
@@ -1,11 +1,16 @@
 package br.com.rr.mastertech.cliente;
 
+import br.com.rr.mastertech.cliente.configuration.RibbonConfiguration;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
+import org.springframework.cloud.netflix.ribbon.RibbonClients;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
 @EnableFeignClients
+@RibbonClients(defaultConfiguration = RibbonConfiguration.class)
+@EnableCircuitBreaker
 public class ClienteServiceApplication {
 
 	public static void main(String[] args) {
